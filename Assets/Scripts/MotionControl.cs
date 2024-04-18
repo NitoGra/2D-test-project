1c2d999be1c9e8353af4905832c73446cdd59026
ArkNito <b-teme@mail.ru>
2024-04-18 20:39:40 +0300

    Исправление ошибок

--- Assets/Scripts/MotionControl.cs
+++ Assets/Scripts/MotionControl.cs
@@ -4,16 +4,18 @@ using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class MotionControl : MonoBehaviour
 {
+	private const string Horizontal = "Horizontal";
+	private const KeyCode JumpKey = KeyCode.W;
+
 	[SerializeField] private float _speed;
 	[SerializeField] private Animator _animator;
 	[SerializeField] private float _jumpSpeed;
 	[SerializeField] private LayerMask _groundMask;
-	[SerializeField] private CircleCollider2D _groundTrigger;
-	[SerializeField] private Rigidbody2D _rigidbody2D;
 
+	private CircleCollider2D _groundTrigger;
+	private Rigidbody2D _rigidbody2D;
 	private Vector2 _moveVector;
 	private SpriteRenderer _spriteRenderer;
-
 	public bool IsGrounded => WasGrounded();
 
 	private bool WasGrounded()
@@ -23,57 +25,42 @@ public class MotionControl : MonoBehaviour
 
 	private void Start()
 	{
-		//_groundTrigger = GetComponentInChildren<CircleCollider2D>();
-		//_rigidbody2D = GetComponent<Rigidbody2D>();
-		_animator.Play("stayIdle");
+		_groundTrigger = GetComponentInChildren<CircleCollider2D>();
+		_rigidbody2D = GetComponent<Rigidbody2D>();
 		_spriteRenderer = GetComponent<SpriteRenderer>();
 	}
 
 	private void Update()
 	{
-		if (Input.GetKeyUp(KeyCode.W) && IsGrounded)
+		if (Input.GetKeyUp(JumpKey) && IsGrounded)
 		{
-			Debug.Log("Ïðûæîê");
 			_rigidbody2D.AddForce(transform.up * _jumpSpeed, ForceMode2D.Impulse);
 			_animator.Play(PlayerAnimatorData.Params.jump);
 		}
+	}
 
-		if (IsGrounded == false)
+	private void FixedUpdate()
+	{
+		_moveVector.x = Input.GetAxis(Horizontal);
+
+		if (IsGrounded && _moveVector.x != 0)
+		{
+			_animator.Play(PlayerAnimatorData.Params.run);
+		}
+		else if(IsGrounded == false)
 		{
 			_animator.Play(PlayerAnimatorData.Params.jump);
 			return;
 		}
 
-		_moveVector.x = Input.GetAxis("Horizontal");
-		/*
-		if (IsGrounded && _moveVector.x != 0)
-		{
-			_animator.Play("run");
-		}*/
-
-		if (_moveVector.x > 0 && IsGrounded)
-		{
+		if (_moveVector.x > 0)
 			_spriteRenderer.flipX = false;
-			_animator.Play("run");
-
-		}
-		else if (_moveVector.x < 0 && IsGrounded)
-		{
+		else if (_moveVector.x < 0)
 			_spriteRenderer.flipX = true;
-			
-			_animator.Play("run");
-		}
 		else
-			_animator.Play("stayIdle");
+			_animator.Play(PlayerAnimatorData.Params.stayIdle);
 
 		_rigidbody2D.velocity = new Vector2(Input.GetAxis("Horizontal") * _speed, _rigidbody2D.velocity.y);
-
-		//_rigidbody2D.MovePosition(_rigidbody2D.position + _moveVector * _speed * Time.fixedDeltaTime);
-	}
-
-	private void FixedUpdate()
-	{
-
 	}
 }
 
@@ -82,6 +69,8 @@ public static class PlayerAnimatorData
 	public static class Params
 	{
 		public static readonly int jump = Animator.StringToHash(nameof(jump));
+		public static readonly int run = Animator.StringToHash(nameof(run));
+		public static readonly int stayIdle = Animator.StringToHash(nameof(stayIdle));
 
 	}
 }
